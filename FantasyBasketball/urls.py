01ca3021d62be896166bb64c6da3c5c605071b53
Xiao Zhang <xz1661@nyu.edu>
2017-05-11 01:58:12 -0400

    session complete

--- FantasyBasketball/urls.py
+++ FantasyBasketball/urls.py
@@ -11,8 +11,8 @@ from users.views import LoginView, RegisterView, UserInfoView, UserRulesView, Us
 urlpatterns = [
     url(r'^admin/', admin.site.urls),  # 后台管理页面
     url(r'^captcha/', include('captcha.urls')),  # 验证码
-    # url(r'^$', LoginView.as_view(), name='log_in'),  # 用户登录界面(主页)
-    url(r'^$', users_views.log_in, name='log_in'),  # 用户登录界面(主页)
+    url(r'^login/$', LoginView.as_view(), name='log_in'),  # 用户登录界面(主页)
+    # url(r'^$', users_views.log_in, name='log_in'),  # 用户登录界面(主页)
     url(r'^register/$', RegisterView.as_view(), name='register'),  # 用户注册页面
     # url(r'^register/$', users_views.register, name='register'),  # 用户注册页面
     url(r'^user_center/info/$', UserInfoView.as_view(), name='user_info'),  # 用户中心信息页面
@@ -20,5 +20,6 @@ urlpatterns = [
     url(r'^user_center/rules/$', UserRulesView.as_view(), name='user_rules'),  # 用户中心游戏规则页面
     # url(r'^user_center/rules/$', users_views.usercenter_rules, name='user_rules'),  # 用户中心游戏规则页面
     url(r'^home$', HomeView.as_view(), name='home'),  # 游戏大厅页面
+    url(r'^$', HomeView.as_view(), name='home'),  # 游戏大厅页面
     url(r'^select_team/$', TeamView.as_view(), name='select_team'),  # 组建队伍页面
 ]
