31ceab7df2fb19f8d8e89a8ffc13b71d1ee59be4
Xiao Zhang <xz1661@nyu.edu>
2017-05-13 14:53:23 -0400

    complete

--- apps/game/nba_data_api.py
+++ apps/game/nba_data_api.py
@@ -20,21 +20,36 @@ params = urllib.urlencode({
 
 
 def getNewNbaData(date, playerID):
-    conn = httplib.HTTPSConnection('api.fantasydata.net')
-    requestData = "/v3/nba/stats/JSON/PlayerGameStatsByPlayer/{0}/{1}?%s".format(date, playerID)
-    conn.request("GET", requestData % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    jsondata = json.loads(data)
+    try:
+        conn = httplib.HTTPSConnection('api.fantasydata.net')
+        requestData = "/v3/nba/stats/JSON/PlayerGameStatsByPlayer/{0}/{1}?%s".format(date, playerID)
+        conn.request("GET", requestData % params, "{body}", headers)
+        response = conn.getresponse()
+        data = response.read()
+        jsondata = json.loads(data)
 
-    playerStatus = {
-        'name': jsondata['Name'],
-        'fantasy_score': jsondata['FantasyPoints'],
-        'isGameOver': jsondata['IsGameOver']
-    }
+        playerStatus = {
+            'name': jsondata['Name'],
+            'fantasy_score': jsondata['FantasyPoints'],
+            'isGameOver': jsondata['IsGameOver']
+        }
 
-    # Send data to sqs
-    queue.send_message(MessageBody="NewNbaData", MessageAttributes=playerStatus)
+        # Send data to sqs
+        queue_data = {
+            'name': {'DataType': 'String', 'StringValue': jsondata['Name']},
+            'isGameOver': {'DataType': 'String', 'StringValue': str(jsondata['IsGameOver'])},
+            'TotalReboundsPercentage': {'DataType': 'String', 'StringValue': str(jsondata['TotalReboundsPercentage'])},
+            'Points': {'DataType': 'String', 'StringValue': str(jsondata['Points'])},
+            'Assists': {'DataType': 'String', 'StringValue': str(jsondata['Assists'])},
+            'Rebounds': {'DataType': 'String', 'StringValue': str(jsondata['Rebounds'])},
+            'AssistsPercentage': {'DataType': 'String', 'StringValue': str(jsondata['AssistsPercentage'])},
+            'FreeThrowsPercentage': {'DataType': 'String', 'StringValue': str(jsondata['FreeThrowsPercentage'])},
+            'TwoPointersPercentage': {'DataType': 'String', 'StringValue': str(jsondata['TwoPointersPercentage'])},
+            'ThreePointersPercentage': {'DataType': 'String', 'StringValue': str(jsondata['ThreePointersPercentage'])}
+        }
+        queue.send_message(MessageBody="NewNbaData", MessageAttributes=queue_data)
 
-    return playerStatus
-
+        conn.close()
+        return playerStatus
+    except Exception as e:
+        print("[Errno: {0}]".format(e.message))
